3d4ba1a536a58d363a31e68b0b00683b3dc9020f
quoyle <quoyle@163.com>
2017-12-01 17:55:45 +0800

    update

--- database/seeds/OptionsTableSeeder.php
+++ database/seeds/OptionsTableSeeder.php
@@ -45,15 +45,15 @@ class OptionsTableSeeder extends Seeder
             [
                 'name' => '身份证号码',
                 'type' => 'idcard',
-                'key' => 'idcard',
+                'key' => 'id_card',
                 'placeholder' => '身份证号码',
                 'rule' => [
                     'required',
                     'regex:/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}$)/',
                 ],
                 'messages' => [
-                    'idcard.required' => '身份证号码必填',
-                    'idcard.regex' => '身份证号码不正确'
+                    'id_card.required' => '身份证号码必填',
+                    'id_card.regex' => '身份证号码不正确'
                 ]
             ],
             [
